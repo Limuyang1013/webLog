bc8e03e055af8557feb687e090f63da87b263ef5
Banri Cho <i@banri.me>
2015-12-29 21:58:29 +0800

    no message

---
+++ demos/JS/getType.js
@@ -0,0 +1,20 @@
+let getType = arg => {
+    let t;
+
+    if (arg === null) {
+        t = 'null';
+    } else if (typeof arg !== 'object') {
+        t = typeof arg;
+    } else if (Array.isArray(arg)) {
+        t = 'array';
+    } else {
+        t = 'object';
+    }
+
+    console.log('Type is ' + t);
+}
+
+getType([1, 2, 3, 4]);
+getType({
+    1: 'hello'
+});
