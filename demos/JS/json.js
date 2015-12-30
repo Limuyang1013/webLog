78ab48cc6637be002e9fdb5e399a733ba7ebc356
Banri Cho <i@banri.me>
2015-12-30 16:46:35 +0800

    no message

---
+++ demos/JS/json.js
@@ -0,0 +1,33 @@
+var xiaoming = {
+    name: '小明',
+    age: 14,
+    gender: true,
+    height: 1.65,
+    grade: null,
+    'middle-school': '\"W3C\" Middle School',
+    skills: ['JavaScript', 'Java', 'Python', 'Lisp']
+};
+
+// var log = JSON.stringify(xiaoming);
+// console.log(log);
+
+// var log = JSON.stringify(xiaoming, null, '  ');
+// console.log(log);
+
+// var log = JSON.stringify(xiaoming, ['name', 'height', 'skills'], '  ');
+// console.log(log);
+
+function fn(key, value) {
+    if (value === '小明') {
+        value = '小黄'
+    }
+
+    if (key === 'age') {
+        value = '18'
+    }
+
+    return value;
+}
+
+var log = JSON.stringify(xiaoming, fn, '  ');
+console.log(log);
