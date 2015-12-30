473f8a88e2da334bc3e47aa38b51b0fecb386785
Banri Cho <i@banri.me>
2015-12-30 20:11:18 +0800

    no message

--- demos/JS/OOP.js
+++ demos/JS/OOP.js
@@ -24,7 +24,42 @@ function createStu(name) {
 
 var xiaoming = createStu('XIAOMING');
 
-console.log(xiaoming.cls);
+// console.log(xiaoming.cls);
 
 // ---------- fn ----------
+var Stu = function(name) {
+    this.name = name;
+    this.sayHi = function() {
+        return 'Hello, ' + this.name + '!';
+        // 传入 name 并使用 name 提示 undefined
+        // 直接使用 name，会产生闭包
+        // 所以应该使用 this.name
+    }
+}
 
+var xm = new Stu('XIAOMING');
+
+// console.log(xm.sayHi());
+// console.log(xm);
+
+// ---------- fn.prototype ----------
+var oopStu = function(name) {
+    this.name = name;
+}
+
+// oopStu.prototype.sayHi = function() {
+//     return 'Hello, ' + this.name + '!';
+// }
+
+oopStu.prototype = {
+    constructor: oopStu,
+    sayHi: function() {
+        return 'Hello, ' + this.name + '!';
+    }
+}
+
+var xiaohong = new oopStu('XIAO HONG');
+
+console.log(xiaohong.sayHi());
+console.log(xiaohong.constructor === oopStu.prototype.constructor); // true
+console.log(oopStu.prototype.constructor === oopStu); // true
