5eec04b33dff409c677c8a77c5a32f4e312e22d7
Banri Cho <i@banri.me>
2016-01-06 22:31:09 +0800

    no message

--- demos/ES6/ES6Function.js
+++ demos/ES6/ES6Function.js
@@ -1,5 +1,8 @@
-function sayHi(word, who = 'World') {
+function sayHi(word = 'Hello', who = 'World') {
     console.log(word + ' ' + who + '!');
 }
 
-sayHi('Hello');
+sayHi(); // Hello World!
+sayHi('Hi'); // Hi World!
+sayHi(undefined, 'Banri'); // Hello Banri!
+sayHi('你好', '世界'); // 你好 世界!
