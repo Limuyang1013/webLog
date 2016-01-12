3dd07e86724c76310707610e896ba20ae5042122
Banri Cho <i@banri.me>
2016-01-12 18:26:34 +0800

    no message

--- demos/OOP/Object.js
+++ demos/OOP/Object.js
@@ -61,26 +61,38 @@
 // console.log(des.enumerable); // true
 
 
-var book = {};
+// var book = {};
 
-Object.defineProperties(book, {
-    _year: {
-        value: 2004
-    },
-    edition: {
-        configurable: true,
-        enumerable: true,
-        value: 1
-    },
-    year: {
-        get: function() {
-            return this._year;
-        },
-        set: function(newValue) {
-            if (newValue > 2004) {
-                this._year = newValue;
-                this.edition += newValue - 2004;
-            }
-        }
-    }
-});
+// Object.defineProperties(book, {
+//     _year: {
+//         writable: true,
+//         value: 2004
+//     },
+//     edition: {
+//         writable: true,
+//         value: 1
+//     },
+//     year: {
+//         get: function() {
+//             return this._year;
+//         },
+//         set: function(newValue) {
+//             if (newValue > 2004) {
+//                 this._year = newValue;
+//                 this.edition += newValue - 2004;
+//             }
+//         }
+//     }
+// });
+
+// book.year = 2015;
+// console.log(book.edition); // 12
+
+
+var obj = {};
+
+Object.defineProperty(obj,'name',{
+    set: function() {},
+    get: function() {},
+    writable: true
+}); // TypeError: Invalid property.  A property cannot both have accessors and be writable or have a value, #<Object>
