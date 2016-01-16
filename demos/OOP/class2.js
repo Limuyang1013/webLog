9bb59638b1645f2e8c06667e77858d86dd6188d3
Banri Cho <i@banri.me>
2016-01-16 20:14:15 +0800

    no message

---
+++ demos/OOP/class2.js
@@ -0,0 +1,27 @@
+// class Parent {
+//     static fn() {
+//         if (new.target !== undefined) {
+//             console.log('Hello');
+//         };
+
+//         console.log('World');
+//     };
+// };
+
+// var child = new Parent();
+
+// Parent.fn(); // World
+
+
+class Parent {
+    constructor() {
+        console.log(new.target);
+    };
+};
+
+class Child extends Parent {
+
+};
+
+var a = new Parent(); // [Function: Parent]
+var b = new Child(); // [Function: Child]
