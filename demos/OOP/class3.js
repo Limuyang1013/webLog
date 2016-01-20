b1310d7482a5a815602d60442ccf7ad8182ecb19
Banri Cho <i@banri.me>
2016-01-21 01:39:57 +0800

    no message

---
+++ demos/OOP/class3.js
@@ -0,0 +1,50 @@
+class Person {
+    constructor() {
+        this.fn = function() {
+            console.log('on Person');
+        };
+    };
+
+    fun() {
+        console.log('on Person.prototype');
+    };
+};
+
+class Student extends Person {
+    constructor() {
+        super();
+
+        console.log(super.__proto__ === Student.prototype);
+        console.log(super.fn);
+        console.log(super.fun);
+    };
+};
+
+var banri = new Student();
+
+// function Person() {
+//     this.fn = function() {
+//         console.log('on Person');
+//     };
+// };
+
+// Person.prototype.fun = function() {
+//     console.log('on proto');
+// };
+
+// function Student() {
+//     Person.call(this);
+
+//     if (Student.fn) {
+//         console.log('has');
+//     };
+// };
+
+// Student.prototype = {
+//     __proto__: Person.prototype,
+//     constructor: Student
+// };
+
+// var banri = new Student();
+
+// banri.fn()
