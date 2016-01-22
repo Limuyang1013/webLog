f1ffd63ffb9a651c109884eff57745cfc899c6b7
Banri Cho <i@banri.me>
2016-01-22 14:10:37 +0800

    no message

--- demos/OOP/class3.js
+++ demos/OOP/class3.js
@@ -1,48 +1,61 @@
-// class Person {
-//     constructor() {
-//         this.fn = function() {
-//             console.log('on Person');
-//         };
-//     };
+class Person {
+    constructor() {
+        this.fn1 = function() {
+            console.log('on Person');
+        };
+    };
 
-//     fun() {
-//         console.log('on Person.prototype');
-//     };
-// };
-
-// class Student extends Person {
-//     constructor() {
-//         super();
-
-//         console.log(super.__proto__ === Student.prototype);
-//         console.log(super.fn);
-//         console.log(super.fun);
-//     };
-// };
-
-// var banri = new Student();
-
-function Person() {
-    this.fn = function() {
-        console.log('on Person');
+    fn2() {
+        console.log('on Person.prototype');
     };
 };
 
-Person.prototype.fun = function() {
-    console.log('on proto');
-};
+class Student extends Person {
+    constructor() {
+        super();
 
-function Student() {
-    Person.call(this);
-};
+        console.log(this.__proto__ === Student.prototype); // true
+        console.log(super.__proto__ === Student.prototype); // true
 
-Student.prototype = {
-    __proto__: Person.prototype,
-    constructor: Student
+        console.log(super); // SyntaxError: 'super' keyword unexpected here
+    };
 };
 
 var banri = new Student();
 
-banri.fn();
-banri.fun();
-console.log(banri.__proto__ === Student.prototype);
+// function Person() {};
+
+// Person.prototype.fun = function() {
+//     console.log('on proto');
+// };
+
+// function Student() {
+//     Person.call(this);
+
+//     console.dir(this);
+//     console.log(this.__proto__ === Student.prototype);
+// };
+
+// Student.prototype = {
+//     __proto__: Person.prototype,
+//     constructor: Student
+// };
+
+// var banri = new Student();
+
+// banri.fn();
+// banri.fun();
+// banri.fn2();
+// console.log(banri.__proto__ === Student.prototype);
+
+
+// function A() {
+//     console.log(123);
+// };
+
+// function B() {
+//     console.log(this);
+//     this();
+// };
+
+// B.apply(A);
